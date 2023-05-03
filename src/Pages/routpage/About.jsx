01b7176235c4e4c29b6fa75df1394bcd4636fed0
abu-jafor <abujaforwd@yahoo.com>
2023-05-03 10:46:04 +0600

    blog q&an

--- src/Pages/routpage/About.jsx
+++ src/Pages/routpage/About.jsx
@@ -1,7 +1,41 @@
 import React from "react";
 
 const About = () => {
-  return <div>Abot Pages in Protected Route</div>;
+  return (
+    <div>
+      <div>
+        <h1>
+          Tell us the differences between uncontrolled and controlled
+          components.
+        </h1>
+        <p>
+          These components rely on props passed down from the parent component
+          to update their state and behavior. Uncontrolled components refer to
+          components that manage their own state internally.
+        </p>
+      </div>
+      <div>
+        <h1>How to validate React props using PropTypes</h1>
+        <p>
+          PropTypes is a type-checking library for React that allows you to
+          ensure the correctness of your component's props. By specifying the
+          expected types and any additional requirements, you can catch
+          potential errors early on in development and improve the reliability
+          and maintainability of your code. Was this response better or worse?
+          Better Worse Same
+        </p>
+      </div>
+      <div>
+        <h1>Tell us the difference between nodejs and express js.</h1>
+        <p>
+          Node.js is a runtime environment that allows JavaScript code to run
+          outside the browser, while Express.js is a web application framework
+          built on top of Node.js, providing additional features and simplifying
+          the development of web applications.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default About;
